c59d05dd475777c379bc12b164fdb428cd395edf
SliDeR <rsheveljov@gmail.com>
2020-01-28 00:08:08 +0200

    Big Update

---
+++ script/productCards.js
@@ -0,0 +1,49 @@
+document.addEventListener('DOMContentLoaded', () => {
+
+  let cardsSection = document.querySelector('section')
+  let cardsData = '../script/db.json'
+
+
+  const createCard = (id, img, price, about) => {
+    let cardBox = document.createElement('div')
+    cardBox.className = 'cardBox'
+    cardBox.innerHTML = `<div class="img">
+                            <img src=${img} alt="key">
+                          </div>
+                          <div class="text">
+                            <p>${about}</p>
+                          </div>
+                          <div class="price">
+                            <span>ID: ${id}</span>
+                            <p class="itemPrice">${price} &#8364;</p>
+                         </div>`
+    return cardBox
+  }
+  
+  
+  let request = new XMLHttpRequest()
+  request.open('GET', cardsData)
+  request.responseType = 'json'
+  request.send()
+
+  request.onload = function() {
+    let cardsData = request.response
+    for (let i = 0; i < cardsData.length; i++) {
+      let id = cardsData[i].id
+      let image = cardsData[i].image
+      let price = cardsData[i].price
+      let about = cardsData[i].about
+      
+      cardsSection.append(createCard(id, image, price, about))
+      }
+    }
+
+
+    
+    
+      
+})
+  
+  
+  
+
